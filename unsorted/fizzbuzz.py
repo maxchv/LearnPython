d229e7f45028308e45f25b749f40b1f9d7f120a2
Maxim <maxshaptala@gmail.com>
2016-08-19 17:10:59 +0300

    Добавлена презентация о циклах и папка unsorted

---
+++ unsorted/fizzbuzz.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+"""
+Write a program to print fizz, buzz or fizzbuzz if one of the conditions is meet.
+If the number is a multiple of 3, print fizz; a multiple of 5, print buzz;
+and if a multiple of both 3 and 5, print fizzbuzz.
+
+https://github.com/kracekumar/pythonbyexamplestutorial/blob/master/apr-workshop/examples/02-fizz-buzz-modified.py
+"""
+
+# Below is function definition
+def main():
+    # Create variable named num.
+    # HINT:
+    # if cond:
+    #     pass
+    # elif cond:
+    #     pass
+    # else:
+    #     pass
+    pass
+
+
+# __name__ holds the name of the current module
+if __name__ == "__main__":
+	main() # call main function	
