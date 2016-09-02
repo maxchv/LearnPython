1b536f89964767a9771691eb7201e21cef5d2204
Maxim <maxshaptala@gmail.com>
2016-09-02 17:59:32 +0300

    добавлены домашки

---
+++ week04/functions.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+def square(num):
+	"""
+	Задание 1. Напишите функцию, которая возвращает
+	квадрат числа num
+	
+	>>> square(1)
+	1
+	>>> square(2)
+	4
+	>>> square(0)
+	0
+	>>> square(-1)
+	1
+	>>> square(5)
+	25
+	"""
+	pass
+
+def cube(num):
+	"""
+	Задание 2. Напишите функцию, которая возвращает
+	куб числа num
+	
+	>>> cube(1)
+	1
+	>>> cube(2)
+	8
+	>>> cube(0)
+	0
+	>>> cube(-1)
+	-1
+	>>> cube(5)
+	125
+	"""
+	pass
+
+# Given a list of numbers return list of squares.
+def listOfSquare(lst):
+	"""
+	Задание 3. Напишите функцию, которая принимает
+	список чисел и возвращает список квадратов этих чисел
+	
+	>>> listOfSquare([1])
+	[1]
+	>>> listOfSquare([1, 2])
+	[1, 4]
+	>>> listOfSquare([0, 5, 3])
+	[0, 25, 3]
+	>>> listOfSquare([-1, 1, 2])
+	[1, 1, 4]
+	>>> listOfSquare([5, 6, 7])
+	[25, 36, 49]
+	"""
+	pass
+
+# Given a list of numbers filter even numbers
+
+# test
+
+# Combine map, filter, sum.
+# Given a list of numbers, find all even numbers and find sum of squares
+
+# Test
+
+def main():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    main()
---
+++ week04/homework07.rst
@@ -0,0 +1,16 @@
+===================
+Домшанее задание 07
+===================
+
+Материалы занятия:  https://github.com/maxchv/LearnPython/tree/master/week04
+
+Слайды:	            https://github.com/maxchv/LearnPython/tree/master/week04/07.Functions.pptx
+
+Домашнее задание:   https://github.com/maxchv/LearnPython/blob/master/week04/functions.py
+
+Листинг:	  	    https://github.com/maxchv/LearnPython/blob/master/week04/examples/
+
+Примеры на занятии: https://github.com/maxchv/LearnPython/blob/master/week04/examples/
+
+Видео: 				
+
