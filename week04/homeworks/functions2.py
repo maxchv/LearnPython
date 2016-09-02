76044c058c5e3e0c9746a4250d8d57ede745215b
Maxim <maxshaptala@gmail.com>
2016-09-03 01:02:15 +0300

    добавлены два задания

--- week04/homeworks/functions2.py
+++ week04/homeworks/functions2.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+# Write a function flatten which will flatten the nested list
+
+# Test
+
+# write a function which will compute factorial of n, using recursion
+
+# Test
 
 def main():
     import doctest
