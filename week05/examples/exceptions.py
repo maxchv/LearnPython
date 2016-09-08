4c633ce4e7fe1f0d1bf8d701cbbf210ae3afb864
Maxim <maxshaptala@gmail.com>
2016-09-08 16:15:43 +0300

    добавлены материалы

---
+++ week05/examples/exceptions.py
@@ -0,0 +1,63 @@
+## [subcode]
+def add(x, y):
+    return x + y
+    
+## [subcode]
+def add(x, y):
+    try:
+        return x + y
+    except:
+        return "Ошибка вычисления"
+
+## [subcode]
+
+x = int(input("Введите число :"))
+        
+## [subcode]
+
+try:
+    x = int(input("Введите число: "))
+except ValueError:
+    print("Вы должны ввести число.")
+except KeyboardInterrupt:
+    print("Вы отменили операцию.")
+    
+## [subcode]
+
+try:
+    x = int(input("Введите число: "))
+except ValueError:
+    print("Вы должны ввести число.")
+except KeyboardInterrupt:
+    print("Вы отменили операцию.")
+else:
+    print("Вы ввели число", x)
+
+## [subcode]
+
+try:
+    x = int(input("Введите число: "))
+except ValueError:
+    print("Вы должны ввести число.")
+except KeyboardInterrupt:
+    print("Вы отменили операцию.")
+else:
+    print("Вы ввели число", x)
+finally:
+    print("Работа программы завершена")
+ 
+## [subcode]
+    
+import builtins
+import inspect
+for type_name in dir(builtins):
+    _type = getattr(builtins, type_name)    
+    if inspect.isclass(_type):
+        try:
+            if isinstance(_type(), Exception):
+                print(type_name)
+        except:
+            pass
+            
+
+        
