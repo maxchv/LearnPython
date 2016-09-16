5e8d45e55283745e0d229f3d12b86882f7e22272
Maxim <maxshaptala@gmail.com>
2016-09-16 21:16:32 +0300

    добавлены материалы занятия

---
+++ week06/homework/lambda.py
@@ -0,0 +1,11 @@
+# 1. Write a program which can filter even numbers in a list by using filter function. The list is: [1,2,3,4,5,6,7,8,9,10].
+
+# 2. Write a program which can map() to make a list whose elements are square of elements in [1,2,3,4,5,6,7,8,9,10].
+
+# 3. Write a program which can map() and filter() to make a list whose elements are square of even number in [1,2,3,4,5,6,7,8,9,10].
+
+# 4. Write a program which can filter() to make a list whose elements are even number between 1 and 20 (both included).
+
+# 5. Write a program which can map() to make a list whose elements are square of numbers between 1 and 20 (both included).
+
+# 6. Write a function that returns a list of n functions, such that each one, when called, will return the input value, incremented by an increasing number.
