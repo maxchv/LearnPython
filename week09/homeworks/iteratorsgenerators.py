06402c19bf17ac6b9f79eddf05094b950dd8a348
Maxim <maxshaptala@gmail.com>
2016-10-13 21:36:00 +0300

    добавлено

--- week09/homeworks/iteratorsgenerators.py
+++ week09/homeworks/iteratorsgenerators.py
@@ -55,14 +55,13 @@ class SquareIterator:
 
 class Range:
     """
-    Задание 2
+    Задание 3
 
-    Реализовать итератор SquareIterator, который возвращает
-    квадраты чисел от 1 до 10.
+    Реализовать итератор Range - аналог встроенной функции range
 
     Перегрузите методы __next__ и __iter__
 
-    >>> it = Range()
+    >>> it = Range(10)
     >>> for i in range(10):  next(it)
     0
     1
@@ -78,80 +77,28 @@ class Range:
     Traceback (most recent call last):
     ...
     StopIteration
+    >>> it = Range(2, 5)
+    >>> next(it)
+    2
+    >>> next(it)
+    3
+    >>> next(it)
+    4
+    >>> next(it)
+    Traceback (most recent call last):
+    ...
+    StopIteration
+    >>> it = Range(2, 5, 2)
+    >>> next(it)
+    2
+    >>> next(it)
+    4
+    >>> next(it)
+    Traceback (most recent call last):
+    ...
+    StopIteration
     """
     pass
-
-class Vehicle:
-    """
-
-    """
-    highestVIN = 0
-    def __init__(self, owner):
-        self.__current_speed = 0
-        self.__current_direction = 0
-        self.__owner = owner
-        self.highestVIN += 1
-        self.__VIN = self.highestVIN
-
-    @property
-    def current_direction(self):
-        return self.__current_direction
-
-    @property
-    def owner(self):
-        return self.__owner
-
-    @property
-    def current_speed(self):
-        return self.__current_speed
-
-
-# Write a simple Vehicle class that has fields for (at least)
-# current speed, current direction in degrees, and owner name.
-# class Vehicle {
-#   int currentSpeed;
-#   int currentDirection;
-#   String owner;
-# }
-# Add a static field to your Vehicle class for the highest Vehicle
-# Identification Number issued, and a non-static field that holds each
-# vehicle's ID number.
-# class Vehicle {
-#   int currentSpeed;
-#   int currentDirection;
-#   String owner;
-#   static int highestVIN;
-#   int VIN;
-# }
-# Write a main method for your Vehicle class that creates a
-# few vehicles and prints out their field values. Note that
-# you can also write a separate tester program as well.
-# class Vehicle {
-#
-#   int currentSpeed;
-#   int currentDirection;
-#   String owner;
-#   static int highestVIN;
-#   int VIN;
-#
-#   public static void main(String[] args) {
-#
-#     Vehicle a = new Vehicle();
-#     a.VIN = highestVIN++;
-#     a.owner = "Larry Bird";
-#
-#     Vehicle b = new Vehicle();
-#     b.VIN = highestVIN++;
-#     b.owner = "Mark Jackson";
-#
-#     System.out.println("VIN: " + a.VIN + " belongs to " + a.owner);
-#
-#     System.out.println("VIN: " + b.VIN + " belongs to " + b.owner);
-#
-#     // Note that both cars are stopped, and facing East.
-#
-#   }
-# }
 
 if __name__ == "__main__":
     import doctest
