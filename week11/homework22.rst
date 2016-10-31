15783e0740b6d0a992e7a4faba782d774326879b
Maxim <maxshaptala@gmail.com>
2016-10-31 16:24:51 +0200

    Добавлено домашнее задание и тест

--- week11/homework22.rst
+++ week11/homework22.rst
@@ -8,6 +8,35 @@
 
 Домашнее задание:   
 
+	1) 	Создать проект website
+	2) 	В проект добавить приложение blog
+	3) 	В файле настроек settings.py добавить приложение blog в список приложений проекта
+	4) 	В проекте создать директорию templates
+	5) 	В директории templates создать директорию blog
+	6) 	В файле настроек settings.py прописать путь к директории с шаблонами templates
+	7) 	В директории проекта создать директорию static и static/blog
+	8) 	Добавить настройки в файл settings.py для указания местоположения директории статических файлов
+	9) 	Распаковать архив https://github.com/maxchv/LearnPython/blob/master/week11/homeworks/tempalte.rar 
+		во временную директорию
+	10)	Скопировать директории css, fonts и js в static/blog
+	11) Поместить файлы index.html и post.html в директорию templates/blog
+	12) На базе файлов index.html и post.html создать общий шаблон base.html
+	13) В базовом шаблоне base.html необходимо:
+			а) подгрузить статические файлы
+			б) изменить ссылки в атрибуте href в тегах link и атрибут src в тегах script
+			в) содержимое элемента div с классом col-md-8 заменить на блок {% block content %}{% endblock %}
+	14) В файлах index.html и post.html необходимо:
+			а) наследовать шаблон base.html
+			б) из содердимого оставить только то, что входило в <div class="col-md-8">
+			в) поместить это содержимое в блок content
+			г) Обновить ссылки с post.html на post и с index.html на /blog
+	15) Добавить представление index в файле blog/views.py, которое должно возвращать 
+		отрендеренный шаблон blog/index.html
+	16) Добавить привязку представления index к пути blog/
+	17) Добавить представление post в файле blog/views.py, которое должно возвращать 
+		отрендеренный шаблон blog/post.html
+	18) Добавить привязку представления post к пути blog/post
+
 Примеры на занятии: https://github.com/maxchv/LearnPython/tree/master/week11/examples/website
 		
 
---
+++ week11/homeworks/homework22/tests.py
@@ -0,0 +1,107 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from website import settings
+import os.path
+from blog import views
+
+
+class TestProject(TestCase):
+    def test_project_name(self):
+        self.assertEqual("website", os.path.basename(settings.BASE_DIR),
+                         "Отсутствует проект website")
+
+    def test_application(self):
+        path = os.path.join(settings.BASE_DIR, "blog")
+        self.assertTrue(os.path.exists(path),
+                        "Отстутвует приложение blog")
+        self.assertTrue('blog' in settings.INSTALLED_APPS,
+                        "Не добавлено приложение blog в список INSTALLED_APPS")
+
+    def test_templates_dir(self):
+        path = os.path.join(settings.BASE_DIR, "templates")
+        self.assertTrue(os.path.exists(path),
+                        "Отстутвует директория templates")
+
+        path = os.path.join(path, "blog")
+        self.assertTrue(os.path.exists(path),
+                        "Отстутвует директория templates/blog")
+
+    def test_settings_templates(self):
+        path = os.path.join(settings.BASE_DIR, "templates")
+        self.assertTrue(path in settings.TEMPLATES[0]['DIRS'],
+                        "Не настроен путь к диретории с шаблонами в TEMPLATES[0]['DIRS']")
+
+    def test_exists_static_dirs(self):
+        path = os.path.join(settings.BASE_DIR, "static")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует директория static")
+
+        path = os.path.join(path, "blog")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует директория static/blog")
+
+    def test_settings_static_dir(self):
+        path = os.path.join(settings.BASE_DIR, "static")
+        self.assertTrue(path in settings.STATICFILES_DIRS,
+                        "Отстутсвуют настройки статических файлов в STATICFILES_DIRS")
+
+    def test_exists_css_dir(self):
+        css_path = os.path.join(settings.BASE_DIR, "static", "blog", "css")
+        self.assertTrue(os.path.exists(css_path),
+                        "Отсутствует диретория static/blog/css")
+        path = os.path.join(css_path, "blog-home.css")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/css/blog-home.css")
+        path = os.path.join(css_path, "blog-post.css")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/css/blog-post.css")
+        path = os.path.join(css_path, "bootstrap.css")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/css/bootstrap.css")
+        path = os.path.join(css_path, "bootstrap.min.css")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/css/bootstrap.min.css")
+
+    def test_exists_js_dir(self):
+        js_path = os.path.join(settings.BASE_DIR, "static", "blog", "js")
+        self.assertTrue(os.path.exists(js_path),
+                        "Отсутствует диретория static/blog/js")
+
+        path = os.path.join(js_path, "bootstrap.js")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/js/bootstrap.js")
+
+        path = os.path.join(js_path, "bootstrap.min.js")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/js/bootstrap.min.js")
+
+        path = os.path.join(js_path, "jquery.js")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл static/blog/js/jquery.js")
+
+    def test_exists_fonts_dir(self):
+        fonts_path = os.path.join(settings.BASE_DIR, "static", "blog", "fonts")
+        self.assertTrue(os.path.exists(fonts_path),
+                        "Отсутствует диретория static/blog/fonts")
+
+    def test_exists_templates_file(self):
+        base = os.path.join(settings.BASE_DIR, "templates", "blog")
+        path = os.path.join(base, "base.html")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл templates/blog/base.html")
+        path = os.path.join(base, "index.html")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл templates/blog/index.html")
+        path = os.path.join(base, "post.html")
+        self.assertTrue(os.path.exists(path),
+                        "Отсутствует файл templates/blog/post.html")
+
+
+class TestBlog(TestCase):
+    def test_index_route(self):
+        index = resolve("/blog/")
+        self.assertEqual(index.func, views.index)
+
+    def test_index_route(self):
+        index = resolve("/blog/post/")
+        self.assertEqual(index.func, views.post)
