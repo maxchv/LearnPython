b66f863e1f9d1d28afd7527b505ee5c6cce784c4
Maxim <maxshaptala@gmail.com>
2016-11-05 14:01:44 +0300

    обновлена домашка, изменена структура примеров

--- week12/examples/db/extract_sql.py
+++ week12/examples/db/extract_sql.py
@@ -9,6 +9,19 @@ movies = """1	Toy Story	John Lasseter	1995	81
 9	WALL-E	Andrew Stanton	2008	104
 10	Up	Pete Docter	2009	101"""
 
+north_american_cities = """Guadalajara	Mexico	1500800	20.659699	-103.349609
+Toronto	Canada	2795060	43.653226	-79.383184
+Houston	United States	2195914	29.760427	-95.369803
+New York	United States	8405837	40.712784	-74.005941
+Philadelphia	United States	1553165	39.952584	-75.165222
+Havana	Cuba	2106146	23.05407	-82.345189
+Mexico City	Mexico	8555500	19.432608	-99.133208
+Phoenix	United States	1513367	33.448377	-112.074037
+Los Angeles	United States	3884307	34.052234	-118.243685
+Ecatepec de Morelos	Mexico	1742000	19.601841	-99.050674
+Montreal	Canada	1717767	45.501689	-73.567256
+Chicago	United States	2718782	41.878114	-87.629798"""
+
 
 def generate_insert_sql(text, table):
     sql = 'INSERT INTO {} VALUES '.format(table)
@@ -20,3 +33,4 @@ def generate_insert_sql(text, table):
 
 
 print(generate_insert_sql(movies, 'movies (title, director, year, length_minutes)'))
+print(generate_insert_sql(north_american_cities, "north_american_cities (city,	country	population,	latitude,	longitude)"))
--- week12/homeworks/homework23/movies.py
+++ week12/homeworks/homework23/movies.py
@@ -3,8 +3,10 @@
 
 Работа с базами данных. Язык SQL.
 
-В текущей директории находится файл db.sqlite3 - база данных в формате
-sqlite в которой находится таблица movies:
+В текущей директории должен находится файл db.sqlite3 - база данных в формате
+sqlite которую можно скачать по адресу https://github.com/maxchv/LearnPython/blob/master/week12/homeworks/homework23/db.sqlite3
+
+В базе данных находится таблица movies:
 
 id  title           director            year    length_minutes
 --------------------------------------------------------------
@@ -80,27 +82,17 @@ def task20():
     >>> task20()
     [[1, 'Toy Story', 'John Lasseter', 1995, 81], [2, "A Bug's Life", 'John Lasseter', 1998, 95], [3, 'Toy Story 2', 'John Lasseter', 1999, 93], [7, 'Cars', 'John Lasseter', 2006, 117]]
     """
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute("select * from movies WHERE director like 'John Lasseter'")
-    movies = [list(row) for row in c.fetchall()]
-    c.close()
-    conn.close()
-    return movies
+
+    return []
 
 
 def task21():
     """
-    Найти все фильмы компании Pixar отсортированные в алфавитном порядке
+    Найти все фильмы длительность которыъ более 100 минут
     Вернуть список со списом полей всех фильмов соответствующих заданию
 
     >>> task21()
-    [[1, 'Toy Story', 'John Lasseter', 1995, 81], [2, "A Bug's Life", 'John Lasseter', 1998, 95], [3, 'Toy Story 2', 'John Lasseter', 1999, 93], [7, 'Cars', 'John Lasseter', 2006, 117]]
+    [[5, 'Finding Nemo', 'Andrew Stanton', 2003, 107], [6, 'The Incredibles', 'Brad Bird', 2004, 116], [7, 'Cars', 'John Lasseter', 2006, 117], [8, 'Ratatouille', 'Brad Bird', 2007, 115], [9, 'WALL-E', 'Andrew Stanton', 2008, 104], [10, 'Up', 'Pete Docter', 2009, 101]]
     """
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute("select * from movies WHERE director like 'P'")
-    movies = [list(row) for row in c.fetchall()]
-    c.close()
-    conn.close()
-    return movies
+
+    return []
